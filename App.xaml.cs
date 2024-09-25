625603247bca0b96946b2ccb2b420f42e5e00561
jim <jim@DESKTOP-CTH97A0>
2024-09-24 20:10:11 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace COSC_2100_assingment1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
