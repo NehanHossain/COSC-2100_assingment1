c167616b33434f60ea46a9db6389c8a729be2ef2
NehanHossain <156833031+NehanHossain@users.noreply.github.com>
2024-09-28 16:48:49 -0400

    Applied comments and slight adjustments

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@
 // Project: assignment 1
 // Description:This file contains all the C# code for COSC-2100_assignment1
 
+//inports
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Diagnostics.Metrics;
 using System.Numerics;
@@ -20,54 +21,67 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+//namesspace
 namespace COSC_2100_assingment1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        //Counter that counts up after enter is clicked
         int counter = 0; 
+
+        //contaner for all the numbers inputed
         private List<int> numbers = new List<int>();
 
+
+        //genarates main window
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        //fungction for enter button 
         private void enter_Click(object sender, RoutedEventArgs e)
         {
+            //takes the input cnd checks if everything it is valid
             string theInput = input.Text;
             int number = Validation(theInput); 
 
+            //if number is above or equel to zero it adds to the counter and list while displayng all the information
             if (number >= 0)
             {
                 counter++;
-                float average = Calculation(number);
-                seconderyOutput.Text = "messages per day: " + average.ToString();
+                numbers.Add(number);
                 mainOutput.Text += number + Environment.NewLine; 
                 day.Content = "Day " + counter.ToString();
+
+                //makes shure that the input text box is clear
                 input.Clear();
 
+
+                //if the counter reaches 7 it calculates and displays the avrage of all the numbers and disabels the enter button
                 if (counter == 7)
                 {
+                    float average = (float)numbers.Average();
+                    seconderyOutput.Text = "Avrage messages per day: " + average.ToString();
                     enter.IsEnabled = false;
                     return;
                 }
             }
+
         }
 
+        //Checks if the number is a positive whole number below or equal to 1000
         private static int Validation(string input)
         {
             if (string.IsNullOrEmpty(input))
             {
-                MessageBox.Show("Please enter a valid input.");
+                MessageBox.Show("Please dont leave the input empty.");
             }
             else if (!int.TryParse(input, out int result))
             {
-                MessageBox.Show("Please enter a whole number.");
+                MessageBox.Show("Please enter a possitive whole number.");
             }
-            else if (result >= 1000)
+            else if (result > 1000)
             {
                 MessageBox.Show("Please enter an input that is below 1000.");
             }
@@ -82,20 +96,7 @@ namespace COSC_2100_assingment1
             return -1;
         }
 
-        private float Calculation(int theNumber)
-        {
-            
-            numbers.Add(theNumber);
-            if (numbers.Count == 0)
-            {
-                return (float)theNumber; 
-            }
-            else
-            {
-                return (float)numbers.Average(); 
-            }
-        }
-
+        // clears numbers, input, secondaryOutput and primaryOutput sets counter to zero sets day to zero and enabals enter button
         private void clear_Click(object sender, RoutedEventArgs e)
         {
             numbers.Clear();
@@ -107,6 +108,7 @@ namespace COSC_2100_assingment1
             enter.IsEnabled = true; 
         }
 
+        //exit botton, exits program
         private void exit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
